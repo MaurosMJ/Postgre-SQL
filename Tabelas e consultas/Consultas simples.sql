6e16f820dcde1e852a6d481c2b33504081a3dca6
Mauros Milach Junior <mauros99milach@gmail.com>
2024-10-20 10:05:54 -0300

    feat: Consultas simples.sql

--- Tabelas e consultas/Consultas simples.sql
+++ Tabelas e consultas/Consultas simples.sql
@@ -19,3 +19,20 @@ select 'RG: (' || rg || ') CPF: (' || cpf || ')' as "CPF e RG" from clientes;
 -- Select com limite de registros (Sintaxe especifica do Postgres/MySQL)
 select * from clientes limit 3;
 
+-- Utilizando clausala where
+select nome, data_nascimento from clientes where extract (year from data_nascimento) > 2000;
+
+-- Nomes que começam com 'C'
+select nome from clientes where lower(nome) like 'c%'
+
+-- Entre duas datas
+select nome, data_nascimento from clientes where extract (year from data_nascimento) between 1990 and 2010;
+
+-- Todos campos = NULO
+select nome, rg from clientes where rg is null;
+
+-- Ordenando dados Crescente
+select nome from clientes order by 1;
+
+-- Ordenando dados descendente
+select nome from clientes order by 1 desc;
